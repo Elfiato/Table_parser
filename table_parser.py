5d3b251eca021417fbff3eb8a464fe6f465d45e5
Elfiato <doker912@gmail.com>
2022-05-01 18:32:54 +0500

    Parsing directly from a .docx file

--- table_parser.py
+++ table_parser.py
@@ -1,37 +1,8 @@
-import string
-
-'''
-result_dict - словарь, где ключ - название элемента(может быть без ТУ если ТУ написанов следующей строке),
-значение - количество элементов.
-elem_dict - словарь, где ключ, позиционное обозначение элемента на схеме,
-значение - массив, где:
-0 эл. - название элемента
-1 эл. - количесвто
-2 эл.(если есть)  - примечание(тоже только из той строки, где есть название).
-'''
+from docx import Document
+import xlsxwriter
 
 
-def if_count_zero(values):
-    """
-    Функция вычисляет количество элементов через их позиционное обозначение
-    """
-    if '-' in values[0]:
-        amount_of_el = [el.strip() for el in values[0].split('-')]
-    if ',' in values[0]:
-        amount_of_el = [el.strip() for el in values[0].split(',')]
-    else:
-        amount_of_el = [values[0]]
-    for char in amount_of_el[0]:
-        if char in string.digits:
-            position = amount_of_el[0].index(char)
-    if len(amount_of_el) == 2 and amount_of_el[1]:
-        values[2] = int(amount_of_el[1][position:]) - int(amount_of_el[0][position:]) + 1
-    else:
-        values[2] = 1
-    return values
-
-
-def add_name(values):
+def get_name_prefix(values: str) -> str:
     """
     Функция добавляет тип элеменета к названию
     """
@@ -46,66 +17,119 @@ def add_name(values):
         'WZ': 'Фильтр',
         'X': 'Разъем',
         'XW': 'Разъем',
-        'С': 'Конденсатор'
+        'С': 'Конденсатор',
     }
-    for elem in element_name:
-        if values[0].startswith(elem):
-            if values[1].lower().startswith(element_name[elem].lower()):
-                return values
-            else:
-                values[1] = element_name[elem] + ' ' + values[1]
-                return values
+    for name in element_name:
+        if values.startswith(name):
+            return element_name[name]
+    return ''
 
 
-# название исходного и выходного файла писать сюда
-name_base_file = 'test_input.csv'
-name_write_file = 'test_output.csv'
+def get_dop_info(data: list, row_num: int, result_dict=None, not_visited_flag=True, name='') -> str:
+    """
+    Функция сохраняет примечания элементов
+    """
+    dop_info = ''
+    while True:
+        if data[row_num][3]:
+            dop_info += f'{data[row_num][3]} '
+            row_num += 1
+        else:
+            break
+    if not_visited_flag:
+        return dop_info.strip()
+    else:
+        return (result_dict[name][1] + '\n' + dop_info).strip()
 
-where_tabs = []
-count_letter = -1
-result_dict = {}
-elem_dict = {}
-values = []
 
-with open(name_base_file, 'r', encoding='ANSI') as file:
-    for i in file:
-        # ищем где ';' добавляем в массив
-        if i[0] in string.ascii_letters or i[0] in 'СЕТАХНОВМК':
-            for j in i:
-                count_letter += 1
-                if j == ';':
-                    where_tabs.append(count_letter)
-        # создаем массив значений с названием элементов и их количеством
-        for k in range(len(where_tabs)):
-            if k == 0:
-                values.append(i[:where_tabs[k]].strip())
-            elif k == 3:
-                values.append(i[where_tabs[k]:].strip())
-            else:
-                values.append(i[where_tabs[k - 1] + 1:where_tabs[k]].strip())
-        # если массив не пустой, смотрим чтобы количество элементов было проставлено, иначе вычисляем его
-        # через позиционные обозначения элементов
-        if values:
-            add_name(values)
-            if result_dict.get(values[1], 0) == 0:
-                if values[2] == '':
-                    if_count_zero(values)
-                    result_dict[values[1]] = int(values[2])
+def get_full_el_name(data: list, row_num: int) -> tuple[str, int]:
+    """
+    Функция формирует полное имя элемента, даже если оно находится на нескольких строках
+    """
+    name = ''
+    row_num_start = row_num
+    amount_of_str_in_name = 0
+    prefix = get_name_prefix(data[row_num][0])
+    while True:
+        if data[row_num][1]:
+            name += f'{data[row_num][1]} '
+            row_num += 1
+        else:
+            break
+    if row_num != row_num_start + 1:
+        amount_of_str_in_name = row_num - row_num_start - 1
+    if not name.startswith(prefix):
+        name = prefix + ' ' + name
+    return name.strip(), amount_of_str_in_name
+
+
+def get_amount_of_el(data: list, row_num: int, amount_of_str_in_name: int) -> int:
+    """
+    Функция  считывает количество элементов
+    """
+    try:
+        amount = int(data[row_num + amount_of_str_in_name][2])
+    except ValueError:
+        amount = data[row_num + amount_of_str_in_name][2]
+    return amount
+
+
+def make_el_table(data_table: list) -> dict:
+    """
+    Функция формирует словарь result, где ключ название элемента с учетом его типа, а значение список с количеством
+    элементов данного типа и их примечанием.
+    {'Название элемента: [Количество, примечания]'}
+    """
+    result = {}
+    row_num = 0
+    amount_of_str_in_name = 0
+    while row_num < len(data_table):
+        if len(data_table[row_num]) == 4:
+            if data_table[row_num][1] and data_table[row_num][1] != 'Наименование':
+                name, amount_of_str_in_name = get_full_el_name(data_table, row_num)
+                amount = get_amount_of_el(data_table, row_num, amount_of_str_in_name)
+                if name in result:
+                    result[name] = [result[name][0] + amount, get_dop_info(data_table, row_num, result, False, name)]
                 else:
-                    result_dict[values[1]] = int(values[2])
-            else:
-                if values[2] == '':
-                    if_count_zero(values)
-                    result_dict[values[1]] += int(values[2])
-                else:
-                    result_dict[values[1]] += int(values[2])
-            elem_dict[values[0]] = values[1:]
-        where_tabs = []
-        count_letter = -1
-        values = []
+                    result[name] = [amount, get_dop_info(data_table, row_num)]
+        row_num += 1 + amount_of_str_in_name
+        amount_of_str_in_name = 0
+    return result
 
-sort_key = sorted(list(result_dict))
 
-with open(name_write_file, 'w', encoding='ANSI') as w_file:
-    for key in sort_key:
-        print(f'{key};{result_dict[key]}', file=w_file, end='\n')
+def main():
+    # Названия входного и выходного файлов
+    input_file = 'PE3.docx'
+    output_file = 'test_output.xlsx'
+
+    # считывание данных из исходного перечня
+    f = open(input_file, 'rb')
+    document = Document(f)
+    f.close()
+
+    data = [[]]
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                data[-1].append(cell.text)
+            data.append([])
+
+    result = make_el_table(data)
+
+    # запись в выходной файл
+    workbook = xlsxwriter.Workbook(output_file)
+    worksheet = workbook.add_worksheet()
+
+    r = 0
+    for name in result:
+        worksheet.write(r, 0, name)
+        for i in range(1, 3):
+            worksheet.write(r, i, result[name][i - 1])
+        r += 1
+
+    workbook.close()
+    print('Файл сформирован.')
+
+
+if __name__ == "__main__":
+    main()
